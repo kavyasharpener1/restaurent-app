46335d3c809485fe0a5f6e43b19cf0fca030eba9
kavyasharpener1 <naikkavya201@gmail.com>
2023-09-06 17:14:19 +0530

    deploy

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,27 +5,27 @@ import classes from './AvailableMeals.module.css';
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
+    name: 'Biriyani',
+    description: 'Spiced mix of Chicken and Rice',
+    price: 150,
   },
   {
     id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
+    name: 'Tandoori Chicken',
+    description: 'A South Asian dish of Chicken',
+    price: 200,
   },
   {
     id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
+    name: 'Paneer Tikka',
+    description: 'Chunks of Paneer marinated in spices and grilled',
+    price: 190,
   },
   {
     id: 'm4',
     name: 'Green Bowl',
     description: 'Healthy...and green...',
-    price: 18.99,
+    price: 250,
   },
 ];
 
